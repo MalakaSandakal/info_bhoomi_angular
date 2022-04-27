e3f7fcbdcf82061ab222c5d1de6db30737ff4259
MalakaSandakal <malaka@ceydigital.com>
2022-04-27 12:55:22 +0530

    tools-fucntions

--- src/app/drawtools/drawtools.component.ts
+++ src/app/drawtools/drawtools.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import 'ol/ol.css';
 import 'ol-layerswitcher/dist/ol-layerswitcher.css'
 import Map from 'ol/Map';
@@ -6,14 +6,13 @@ import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import LayerGroup from 'ol/layer/Group';
 import LayerSwitcher from 'ol-layerswitcher';
-import {defaults} from 'ol/control';
-import { BaseLayerOptions,GroupLayerOptions } from 'ol-layerswitcher';
 import OSM from 'ol/source/osm';
 
 // import to draw
-import Draw from'ol/interaction/Draw';
+import Draw from 'ol/interaction/Draw';
 import VectorSource from 'ol/source/Vector';
 import Snap from 'ol/interaction/Snap';
+import Modify from 'ol/interaction/Modify'
 
 @Component({
   selector: 'app-drawtools',
@@ -23,12 +22,18 @@ import Snap from 'ol/interaction/Snap';
 
 export class DrawtoolsComponent implements OnInit {
 
-  map:any= Map;
-  draw:any= Draw;
-  snap:any= Snap;
-  selected_geom_type:string= '';
-  layerSwitcher:any= LayerSwitcher;
-  baseLayer:any= LayerGroup;
+  map: any = Map;
+  draw: any = Draw;
+  snap: any = Snap;
+  edit: any = Modify;
+  selected_geom_type: string = '';
+  layerSwitcher: any = LayerSwitcher;
+  baseLayer: any = LayerGroup;
+  flag_is_polygon_mode_on: boolean = false;
+  stop_tool_icon: string = '<i class ="far fa-stop-circle"></i>'
+  polygon_tool_icon: string = '<i class ="fas fa-draw-polygon"></i>';
+  line_tool_icon: string = '<i class="fas fa-bezier-curve"></i>';
+  point_tool_icon: string = '<i class="fas fa-map-marker-alt"></i>';
 
   constructor() { }
 
@@ -36,7 +41,7 @@ export class DrawtoolsComponent implements OnInit {
     this.initializeMap();
   }
 
-  initializeMap(){
+  initializeMap() {
     this.map = new Map({
       view: new View({
         projection: 'EPSG:4326',
@@ -49,45 +54,55 @@ export class DrawtoolsComponent implements OnInit {
         }),
       ],
       target: 'mymap'
-    }); 
-
-    this.layerSwitcher = new LayerSwitcher({
-      tipLabel: 'Légende', // Optional label for button
-      reverse: true,
-      groupSelectStyle: 'children' // Can be 'children' [default], 'group' or 'none'
     });
-    this.map.addControl(this.layerSwitcher);
-    this.baseLayer = new LayerGroup({
-
-    })
-
   }
 
-  pointToolEnabled(){
-    this.map.removeInteraction(this.draw);
-    this.selected_geom_type = "";
-    this.selected_geom_type = "Point";
-    this.addInteraction(this.selected_geom_type);    
+  pointToolEnabled() {
+    if (this.flag_is_polygon_mode_on == false) {
+      this.point_tool_icon = this.stop_tool_icon;
+      this.selected_geom_type = "Point";
+      this.addInteraction(this.selected_geom_type);
+      this.flag_is_polygon_mode_on = true;
+    } else {
+      this.point_tool_icon = '<i class="fas fa-map-marker-alt"></i>';
+      this.map.removeInteraction(this.draw);
+      this.map.removeInteraction(this.snap);
+      this.flag_is_polygon_mode_on = false;
+    }
   }
 
-  polygonToolEnabled(){
-    this.map.removeInteraction(this.draw);
-    this.selected_geom_type = "";
-    this.selected_geom_type = "Polygon";
-    this.addInteraction(this.selected_geom_type);
+  polygonToolEnabled() {
+    if (this.flag_is_polygon_mode_on == false) {
+      this.polygon_tool_icon = this.stop_tool_icon;
+      this.selected_geom_type = "Polygon";
+      this.addInteraction(this.selected_geom_type);
+      this.flag_is_polygon_mode_on = true;
+    } else {
+      this.polygon_tool_icon = '<i class ="fas fa-draw-polygon"></i>';
+      this.map.removeInteraction(this.draw);
+      this.map.removeInteraction(this.snap);
+      this.flag_is_polygon_mode_on = false;
+    }
   }
 
-  lineToolEnabled(){
-    this.map.removeInteraction(this.draw);
-    this.selected_geom_type = "";
-    this.selected_geom_type = "LineString";
-    this.addInteraction(this.selected_geom_type);
+  lineToolEnabled() {
+    if (this.flag_is_polygon_mode_on == false) {
+      this.line_tool_icon = this.stop_tool_icon;
+      this.selected_geom_type = "LineSting";
+      this.addInteraction(this.selected_geom_type);
+      this.flag_is_polygon_mode_on = true;
+    } else {
+      this.line_tool_icon = '<i class="fas fa-bezier-curve"></i>';
+      this.map.removeInteraction(this.draw);
+      this.map.removeInteraction(this.snap);
+      this.flag_is_polygon_mode_on = false;
+    }
   }
 
-  addInteraction(geom_type:string){   
+  addInteraction(geom_type: string) {
     if (geom_type !== "") {
       this.snap = new Snap({
-        source: new VectorSource(), 
+        source: new VectorSource(),
       });
       this.draw = new Draw({
         source: new VectorSource(),
@@ -95,9 +110,9 @@ export class DrawtoolsComponent implements OnInit {
       });
       this.map.addInteraction(this.draw);
       this.map.addInteraction(this.snap);
-    }else{
+    } else {
       return;
-    }    
-  } 
+    }
+  }
 
 }
--- src/app/openlayers-map/openlayers-map.component.ts
+++ src/app/openlayers-map/openlayers-map.component.ts
@@ -1,13 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import 'ol/ol.css';
-import Map from 'ol/Map';
-import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/osm';
-// import to draw
-import Draw from'ol/interaction/Draw';
-import VectorSource from 'ol/source/Vector';
-import Snap from 'ol/interaction/Snap';
 
 @Component({
   selector: 'app-openlayers-map',
@@ -15,51 +6,11 @@ import Snap from 'ol/interaction/Snap';
   styleUrls: ['./openlayers-map.component.css']
 })
 export class OpenlayersMapComponent implements OnInit {
-  
-  constructor() {
-    
-  }
 
-  ngOnInit(): void {} 
+  constructor() { }
 
-  ngAfterViewInit():void{
+  ngOnInit(): void { }
 
-    // var map = new Map({
-    //   view: new View({
-    //     projection: 'EPSG:4326',
-    //     center: [80.98885377363007, 6.830454104603501],
-    //     zoom: 17,
-    //   }),
-    //   layers: [
-    //     new TileLayer({
-    //       source: new OSM(),
-    //     }),
-    //   ],
-    //   target: 'mymap'
-    // }); 
-
-    // var draw:any = Draw;
-    // var snap:any = Snap;
-
-    // function addInteraction():void {
-    //   const selected_geom_type:string = "Polygon";
-    //   if (selected_geom_type == "Polygon") {
-    //     snap = new Snap({
-    //       source: new VectorSource(), 
-    //     });
-    //     draw = new Draw({
-    //       source: new VectorSource(),
-    //       type: "Polygon",
-    //     });
-    //     map.addInteraction(draw);
-    //     map.addInteraction(snap);
-    //   }else{
-    //     return;
-    //   }
-    // } 
-
-    // addInteraction();
-
-  }
+  ngAfterViewInit(): void { }
 
 }
